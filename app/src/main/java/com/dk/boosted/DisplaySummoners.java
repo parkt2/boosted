4e1017ae2f8d87be4154dee62a15ffe75e8279b4
Tae Park <parkt2@rpi.edu>
2016-08-24 20:54:28 -0400

    -changed XML Layout format from Linear to Grids (room for names) -made changes so that the "Search for Another Summoner" button is visible (but still not yet functional) -added code so that summoner names will now appear next to the champions that they are playing

--- app/src/main/java/com/dk/boosted/DisplaySummoners.java
+++ app/src/main/java/com/dk/boosted/DisplaySummoners.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.view.View;
 
 import org.json.JSONObject;
 
@@ -23,7 +26,9 @@ public class DisplaySummoners extends AppCompatActivity {
         setContentView(R.layout.activity_display_summoners);
         Intent intent = getIntent();
         String[] champFulls = intent.getStringArrayExtra("champFulls");
+        String[] summFulls = intent.getStringArrayExtra("summFulls");
         Log.d("CHAMPFULLS", Arrays.toString(champFulls));
+        Log.d("SUMMFULLS", Arrays.toString(summFulls));
         new DownloadImageTask((ImageView) findViewById(R.id.summoner1))
                 .execute("http://ddragon.leagueoflegends.com/cdn/6.16.2/img/champion/" +
                     champFulls[0]);
@@ -54,6 +59,27 @@ public class DisplaySummoners extends AppCompatActivity {
         new DownloadImageTask((ImageView) findViewById(R.id.summoner10))
                 .execute("http://ddragon.leagueoflegends.com/cdn/6.16.2/img/champion/" +
                         champFulls[9]);
+        //--- text view---
+        TextView sn1 = (TextView) findViewById(R.id.summName1);
+        sn1.setText(summFulls[0]);
+        TextView sn2 = (TextView) findViewById(R.id.summName2);
+        sn2.setText(summFulls[1]);
+        TextView sn3 = (TextView) findViewById(R.id.summName3);
+        sn3.setText(summFulls[2]);
+        TextView sn4 = (TextView) findViewById(R.id.summName4);
+        sn4.setText(summFulls[3]);
+        TextView sn5 = (TextView) findViewById(R.id.summName5);
+        sn5.setText(summFulls[4]);
+        TextView sn6 = (TextView) findViewById(R.id.summName6);
+        sn6.setText(summFulls[5]);
+        TextView sn7 = (TextView) findViewById(R.id.summName7);
+        sn7.setText(summFulls[6]);
+        TextView sn8 = (TextView) findViewById(R.id.summName8);
+        sn8.setText(summFulls[7]);
+        TextView sn9 = (TextView) findViewById(R.id.summName9);
+        sn9.setText(summFulls[8]);
+        TextView sn10 = (TextView) findViewById(R.id.summName10);
+        sn10.setText(summFulls[9]);
 
     }
 // show The Image in a ImageView
@@ -84,6 +110,8 @@ public class DisplaySummoners extends AppCompatActivity {
             bmImage.setImageBitmap(result);
         }
     }
+
+
 
 
 }
